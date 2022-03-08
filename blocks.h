5fe50224db043b5d9b50b63003c08d141d8b1dbd
Noah Vogt <noah@noahvogt.com>
2022-03-08 17:35:20 +0100

    add internet update signal

--- blocks.h
+++ blocks.h
@@ -2,10 +2,9 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
 	{"", "get-volume",					1,		1},
 	{"", "battery",						15,		0},
-	{"", "internet",					5, 		0},
+	{"", "internet",					5, 		3},
 	{"", "brightness",					0,		2},
 	{"", "get-date",					5,		0},
 };
